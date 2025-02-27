db006e73ad7ecec6b33e5b565d57834ef7754ee0
AndreaVDev <andrea.verdura3@gmail.com>
2025-02-27 18:28:54 +0100

    Added tailwind

--- mealsite/settings.py
+++ mealsite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "mealsite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
